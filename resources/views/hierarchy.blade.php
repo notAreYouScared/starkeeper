7fdd204f1c637cd365d4032e492b868f92cb3fa6
notCharles <charles@pelican.dev>
2026-03-02 17:45:16 -0500

    update icon size

--- resources/views/hierarchy.blade.php
+++ resources/views/hierarchy.blade.php
@@ -61,7 +61,7 @@
                                 <a href="{{ route('member.profile', $member) }}"
                                    class="ml-auto shrink-0 text-gray-500 hover:text-blue-400 transition-colors"
                                    aria-label="View {{ $member->name }}'s profile">
-                                    <svg class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                    <svg class="h-6 w-6" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                         <path fill-rule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd"/>
                                     </svg>
                                 </a>
