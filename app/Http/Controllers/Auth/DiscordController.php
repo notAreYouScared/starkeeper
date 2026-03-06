392a11b66e47bdf125327ce38111ab4d42aae361
Charles <charles@pelican.dev>
2026-03-06 06:12:36 -0500

    updates

--- app/Http/Controllers/Auth/DiscordController.php
+++ app/Http/Controllers/Auth/DiscordController.php
@@ -21,38 +21,19 @@ class DiscordController extends Controller
         $discordUser = Socialite::driver('discord')->user();
         $user = User::where('email', $discordUser->getEmail())->first();
 
-        if ($user) {
-            $user->update([
-                'avatar' => $discordUser->getAvatar(),
-            ]);
-        } else {
-            $user = User::create([
-                'discord_id' => $discordUser->getId(),
-                'name'       => $discordUser->getName() ?? $discordUser->getNickname(),
-                'email'      => $discordUser->getEmail(),
-                'avatar'     => $discordUser->getAvatar(),
-            ]);
-        }
+        $user?->update([
+            'avatar' => $discordUser->getAvatar(),
+        ]);
 
         $member = Member::where('discord_id', $discordUser->getId())->first();
 
         if ($member) {
-            $member->update([
-                'name'       => $discordUser->getName() ?? $discordUser->getNickname(),
+            $member?->update([
                 'avatar_url' => $discordUser->getAvatar(),
             ]);
-        } else {
-            Member::create([
-                'discord_id'  => $discordUser->getId(),
-                'name'        => $discordUser->getName() ?? $discordUser->getNickname(),
-                'handle'      => $discordUser->getNickname(),
-                'avatar_url'  => $discordUser->getAvatar(),
-                'profile_url' => DiscordService::profileUrl($discordUser->getId()),
-            ]);
+
+            Auth::login($user, remember: true);
         }
-
-        Auth::login($user, remember: true);
-
-        return redirect()->intended(route('filament.admin.pages.dashboard'));
+        return redirect()->intended(route('home'));
     }
 }
